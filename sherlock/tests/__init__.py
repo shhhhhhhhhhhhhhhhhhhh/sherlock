ba621c28ab5c80a6d082537f8c423a11c98ee9e4
shhhhhhhhhhhhhhhhhhhh <103007920+shhhhhhhhhhhhhhhhhhhh@users.noreply.github.com>
2022-04-04 18:05:12 -0600

    Update __init__.py

--- sherlock/tests/__init__.py
+++ sherlock/tests/__init__.py
@@ -1,4 +1,6 @@
-"""Sherlock Tests
-
+"""Sherlock Tests https://github.com/sherlock-project/sherlock.git
+sggsgsgsgsgyrgtttgfgdg
+yiagbiahrrbfbhkhjfjdsfhkdskhjfakljhfklaffa
 This package contains various submodules used to run tests.
 """
+uydbabgajjjajjfuifsifeuiiuebfiluejfslknfisfwudhher fjudjdjdjdjdrfsiufm iufjisfn isrfs oh 
